72c4e99bf28fdb91a23c88023f2e4082146ace46
Chriss Chris <zenskech@outlook.com>
2020-12-18 23:01:35 +0500

    edit

--- travel-website-react/src/components/Navbar.js
+++ travel-website-react/src/components/Navbar.js
@@ -4,6 +4,7 @@ import { Button } from "./Button";
 
 export const Navbar = () => {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(false);
 
   const handleClick = () => {
     setClick(!click);
