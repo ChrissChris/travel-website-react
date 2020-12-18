5d8ad6b7f843d0b2d5133c605d3fdc28a90c166d
Chriss Chris <zenskech@outlook.com>
2020-12-18 21:51:47 +0500

    edit

--- travel-website-react/src/components/Navbar.js
+++ travel-website-react/src/components/Navbar.js
@@ -1,7 +1,16 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-export const Navbar = () => {
+export default function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => {
+    setClick(!click);
+  };
+  const closeMobileMenu = () => {
+    setClick(false);
+  };
+
   return (
     <>
       <nav className="navbar">
@@ -9,9 +18,45 @@ export const Navbar = () => {
           <Link to="/" className="navbar-logo">
             Travel
           </Link>
-          <div className="menu-item"></div>
+          <div className="menu-item" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li>
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/service"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Service
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/products"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Products
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/sign-up"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Sign Up
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     </>
   );
-};
+}
