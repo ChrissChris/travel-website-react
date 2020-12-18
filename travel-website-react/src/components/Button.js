0fbbd3b4c6e896c3feb58764688c93df233da114
Chriss Chris <zenskech@outlook.com>
2020-12-18 22:34:37 +0500

    Update Button.js

--- travel-website-react/src/components/Button.js
+++ travel-website-react/src/components/Button.js
@@ -4,12 +4,22 @@ import { Link } from "react-router-dom";
 const STYLES = ["btn--primary", "btn-outline"];
 const SIZES = ["btn--medium", "btn--large"];
 
-export const Button = ({ childre, type, onclick, buttonStyle, buttonSize }) => {
+export const Button = ({
+  children,
+  type,
+  onclick,
+  buttonStyle,
+  buttonSize,
+}) => {
   const checkButtonStyles = STYLES.includes(buttonStyle)
     ? buttonStyle
     : STYLES[0];
 
   const checkButtonSizes = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
-  return <Link to="/sign-up" className="btn-mobile"></Link>;
+  return (
+    <Link to="/sign-up" className="btn-mobile">
+      <button>{children}</button>
+    </Link>
+  );
 };
